3946960f7e75480bcd738aa6a2141445e9afe6a1
Rob Haining <rob@tolar.ws>
2020-05-04 17:15:05 -0400

    Initial Commit

---
+++ Stars/StarsView.h
@@ -0,0 +1,13 @@
+//
+//  StarsView.h
+//  Stars
+//
+//  Created by Robert Tolar Haining on 5/4/20.
+//  Copyright © 2020 Robert Tolar Haining. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface StarsView : ScreenSaverView
+
+@end
---
+++ Stars/StarsView.m
@@ -0,0 +1,52 @@
+//
+//  StarsView.m
+//  Stars
+//
+//  Created by Robert Tolar Haining on 5/4/20.
+//  Copyright © 2020 Robert Tolar Haining. All rights reserved.
+//
+
+#import "StarsView.h"
+
+@implementation StarsView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
